50241e0456c26cd0b3e6c5c6256aa3a9aa525dd7
Sam Levi Samson <sam.levi.samson@gmail.com>
2026-06-10 21:55:34 +0530

    added vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,42 @@
+import { defineConfig } from "vite";
+import { resolve } from "path";
+
+export default defineConfig({
+  build: {
+    rollupOptions: {
+      input: {
+        main: resolve(__dirname, "index.html"),
+
+        workouts: resolve(
+          __dirname,
+          "pages/workouts/index.html"
+        ),
+
+        nutrition: resolve(
+          __dirname,
+          "pages/nutrition/index.html"
+        ),
+
+        equipment: resolve(
+          __dirname,
+          "pages/equipment/index.html"
+        ),
+
+        bmi: resolve(
+          __dirname,
+          "pages/bmi/index.html"
+        ),
+
+        cart: resolve(
+          __dirname,
+          "pages/cart/index.html"
+        ),
+
+        profile: resolve(
+          __dirname,
+          "pages/profile/index.html"
+        )
+      }
+    }
+  }
+});
